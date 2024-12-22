9bd510882bbe6f3d84b07e811c0ebdbe57e81304
Neutrino-Sunset <18445931+Neutrino-Sunset@users.noreply.github.com>
2024-12-22 14:18:55 +0000

    Added header, footer, routing and basic styles.

--- aspangularstartapp1.client/src/app/app.component.ts
+++ aspangularstartapp1.client/src/app/app.component.ts
@@ -1,40 +1,17 @@
 import { CommonModule } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-
-interface WeatherForecast {
-   date: string;
-   temperatureC: number;
-   temperatureF: number;
-   summary: string;
-}
+import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from './components/header/header.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
    selector: 'app-root',
-   imports: [CommonModule],
+   imports: [CommonModule, RouterOutlet, HeaderComponent, FooterComponent],
    templateUrl: './app.component.html',
    styleUrl: './app.component.css',
    standalone: true
 })
-export class AppComponent implements OnInit {
-   public forecasts: WeatherForecast[] = [];
-
-   constructor(private http: HttpClient) { }
-
-   ngOnInit() {
-      this.getForecasts();
-   }
-
-   getForecasts() {
-      this.http.get<WeatherForecast[]>('/weatherforecast').subscribe(
-         (result) => {
-            this.forecasts = result;
-         },
-         (error) => {
-            console.error(error);
-         }
-      );
-   }
+export class AppComponent {
 
    title = 'aspangularstartapp1.client';
 }
--- aspangularstartapp1.client/src/app/app.routes.ts
+++ aspangularstartapp1.client/src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { Page1Component } from './components/page1/page1.component';
+import { Page2Component } from './components/page2/page2.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+   { path: '', component: HomeComponent },
+   { path: 'page1', component: Page1Component },
+   { path: 'page2', component: Page2Component },
+];
